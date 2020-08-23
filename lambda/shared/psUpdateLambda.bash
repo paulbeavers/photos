2d37d86c22b922c9f3199a72a7df52d54f7c19e4
pbeavers <paul_beavers@mac.com>
2020-08-23 17:21:35 +0000

    file cleanup

---
+++ lambda/shared/psUpdateLambda.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo psUpdateLambda
+
+source ./lambda.conf
+
+#----------------------------------------------
+# Delete existing zip file
+#----------------------------------------------
+rm psLambda.zip
+
+
+#----------------------------------------------
+# Create new zip file
+#----------------------------------------------
+zip -r psLambda.zip $ZIPFILES
+
+
+#----------------------------------------------
+# Update the code in the Lambda function
+#----------------------------------------------
+aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://psLambda.zip --publish 
+
+
+
